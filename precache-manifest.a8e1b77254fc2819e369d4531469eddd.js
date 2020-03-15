685cb8a6bc119926e0d67938c68a0910c190f7ca
kabeh2 <mattcnk@hotmail.com>
2020-03-15 13:51:12 -0400

    Updates

---
+++ precache-manifest.a8e1b77254fc2819e369d4531469eddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df9273c5dfc389026499c147095dda2",
+    "url": "/node-api-challenge/index.html"
+  },
+  {
+    "revision": "463dc769b9bcdc3cb34b",
+    "url": "/node-api-challenge/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "06705e89bd424d5b9d09",
+    "url": "/node-api-challenge/static/css/main.01dadc13.chunk.css"
+  },
+  {
+    "revision": "463dc769b9bcdc3cb34b",
+    "url": "/node-api-challenge/static/js/2.5f5e7f98.chunk.js"
+  },
+  {
+    "revision": "5a733b4f72d20e4afb54e249ef308592",
+    "url": "/node-api-challenge/static/js/2.5f5e7f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06705e89bd424d5b9d09",
+    "url": "/node-api-challenge/static/js/main.baf8393c.chunk.js"
+  },
+  {
+    "revision": "851d6d90118bfd80095e",
+    "url": "/node-api-challenge/static/js/runtime-main.4629bbf9.js"
+  }
+]);
